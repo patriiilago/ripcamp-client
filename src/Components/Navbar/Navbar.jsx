50749f24bcf37fae7a2b36f5a1f631c082eda8e3
Patri Lago <patrilagoespino@gmail.com>
2024-02-15 17:01:14 +0100

    init NavBar and her css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
-<<<<<<< HEAD
+//importar imagen logo
+import './Navbar.css'
 
+import { Link } from "react-router-dom"
 
-=======
->>>>>>> patri
 const Navbar = () => {
 
 
-}
-<<<<<<< HEAD
+    return (
 
-=======
->>>>>>> patri
+        <nav className='nav'>
+            <Link to={"/"}>
+                <img className="logo-image" alt="logo" />
+            </Link>
+            <Link className='navLink' to={"/projects"}>Projects</Link>
+            <Link className='navLink' to={"/alumni"}>Alumni</Link>
+        </nav>
+    )
+
+}
 export default Navbar
