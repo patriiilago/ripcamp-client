286cebcefd0e38ba8b6e5ddaf66806a1b398d729
Patri Lago <patrilagoespino@gmail.com>
2024-02-15 17:35:51 +0100

    intent Navbar with bootsrap

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,62 @@
 //importar imagen logo
 import './Navbar.css'
-
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 import { Link } from "react-router-dom"
+import React from 'react'
 
 const Navbar = () => {
 
 
+    // return (
+
+    //     <nav className='nav'>
+    //         <Link to={"/"}>
+    //             <img className="logo-image" alt="logo" />
+    //         </Link>
+    //         <Link className='navLink' to={"/projects"}>Projects</Link>
+    //         <Link className='navLink' to={"/alumni"}>Alumni</Link>
+    //     </nav>
+    // )
+
     return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
 
-        <nav className='nav'>
-            <Link to={"/"}>
-                <img className="logo-image" alt="logo" />
-            </Link>
-            <Link className='navLink' to={"/projects"}>Projects</Link>
-            <Link className='navLink' to={"/alumni"}>Alumni</Link>
-        </nav>
-    )
+                    <Nav className="me-auto">
+                        <NavLink to={"/"}>Home</NavLink>
+                        <NavLink to={"/projects"}>Projects</NavLink>
+                        <NavLink to={"/alumni"}>Alumni</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <br />
+            <Navbar bg="primary" data-bs-theme="dark">
+                <Container>
 
+                    <Nav className="me-auto">
+                        <NavLink to={"/"}>Home</NavLink>
+                        <NavLink to={"/projects"}>Projects</NavLink>
+                        <NavLink to={"/alumni"}>Alumni</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <br />
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+
+                    <Nav className="me-auto">
+                        <NavLink to={"/"}>Home</NavLink>
+                        <NavLink to={"/projects"}>Projects</NavLink>
+                        <NavLink to={"/alumni"}>Alumni</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
+
 export default Navbar
