1fb55e94297debfb327f243c81d0f4e7b07c69e1
Teresa <tereac1102@gmail.com>
2024-02-15 12:27:15 +0100

    changing app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [popino, setCount] = useState(0)
 
   return (
     <>
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((popino) => popino + 1)}>
+          count is {popino}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
