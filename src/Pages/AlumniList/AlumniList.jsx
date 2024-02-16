c5dcca0d622c7b6d78dabec31642d6ba45d1b702
Patri Lago <patrilagoespino@gmail.com>
2024-02-16 12:11:48 +0100

    Map.image done

--- src/Pages/AlumniList/AlumniList.jsx
+++ src/Pages/AlumniList/AlumniList.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
 const API_URL = "http://localhost:5005/alumni"
-const AlumniCard = () => {
+
+const AlumniList = () => {
+
+    const [alumnis, setAlumnis] = useState([])
+
+    useEffect(() => loadAlumni(), [])
+
+    const loadAlumni = () => {
+        axios
+            .get(`${API_URL}`)
+            .then(({ data }) => setAlumnis(data))
+            .catch(err => console.log(err))
+    }
+    return (
+        <div className="AlumniList">
+            <h1>Alumni List</h1>
+            <hr />
+            {
+                alumnis.map((alumni) => {
+                    return (
+                        <div className="AlumniCard" key={alumni.id}>
+                            <Link to={`/alumni/${alumni.id}`} >
+                                <img src={alumni.img} alt={alumni.id} />
+
+
+                            </Link>
+
+
+                        </div>
+                    )
+
+                })
+
+            }
+
+
+
+
+        </div>
+
+
+
+
+
+    )
 
 
 
