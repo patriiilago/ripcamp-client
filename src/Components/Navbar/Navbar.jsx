a13a118d1195eaca8a7532f7d71ab4b6a4a631f8
Patri Lago <patrilagoespino@gmail.com>
2024-02-19 17:39:16 +0100

    stetic navbar done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,34 @@
 import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
+import { Nav, Navbar, Row, Col } from 'react-bootstrap'
 import { NavLink, Link } from "react-router-dom"
-import { Navbar } from 'react-bootstrap'
-import Image from 'react-bootstrap/Image';
+
+
 
 const Navigation = () => {
 
+
     return (
-        <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
-            <Container fluid>
-                <NavLink to={"/"}>
-                    <img
-                        src="https://static.vecteezy.com/system/resources/previews/010/595/118/non_2x/headstone-icon-gothic-gravestone-with-skull-flowers-rip-text-creepy-cemetery-grave-black-outline-simple-sketch-isolated-on-white-clipart-for-halloween-decor-logo-apps-print-vector.jpg"
-                        width="50"
-                        height="50"
-                        className="d-inline-block align-top"
-                        alt="RipCamp logo"
-                    />
-                </NavLink>
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
                 <Navbar.Brand >
-                    <p>
-                        RipCamp
-                    </p>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <NavLink to={"/"}>Home</NavLink>
-                        <hr />
-                        <NavLink to={"/projects"}>Projects</NavLink>
-                        <hr />
-                        <NavLink to={"/alumni"}>Alumni</NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    <NavLink to={"/"}>
+                        <img
+                            src="https://static.vecteezy.com/system/resources/previews/010/595/118/non_2x/headstone-icon-gothic-gravestone-with-skull-flowers-rip-text-creepy-cemetery-grave-black-outline-simple-sketch-isolated-on-white-clipart-for-halloween-decor-logo-apps-print-vector.jpg"
+                            width="50"
+                            height="50"
+                            className="d-inline-block align-top"
+                            alt="RipCamp logo"
+                        />
+                    </NavLink>
+                </Navbar.Brand >
+                <Nav className="justify-content-end">
+                    <Link className='nav-link' to={"/"}>Home</Link>
+                    <Link className='nav-link' to={"/projects"}>Projects</Link>
+                    <Link className='nav-link' to={"/alumni"}>Alumni</Link>
+                </Nav>
+            </Container >
+        </Navbar >
     )
 
 }
-
 export default Navigation
